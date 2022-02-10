d80e8031f2d3848735409d831a7820f0bb5a12ca
Aman Rajput <98266121+amangovindrao70@users.noreply.github.com>
2022-02-10 22:34:56 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,22 @@
+// Date - 30/01/2022
+
+#include <stdio.h>
+
+
+int main()
+
+{
+
+    int a, b;
+    printf("enter number a\n");
+    scanf("%d" , &a);
+
+    printf("enter number b\n");
+    scanf("%d" , &b);
+
+    printf("The Sum is %d\n", a+b);
+
+    return 0;
+
+
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+// Date - 29/01/2022
+
+#include <stdio.h>              
+
+int main()
+
+
+{
+    printf("Hello World!");
+    return 0;
+
+
+
+}
