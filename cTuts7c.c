4ec260f5024367894bed7f5836d7abcebb230c68
Aman Rajput <98266121+amangovindrao70@users.noreply.github.com>
2022-02-10 22:42:58 +0530

    Add files via upload

---
+++ cTuts7c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int a, b;
+    a = 34;
+    b = 6;   //(if here 6 replace with 34 then after compiling the result will true means 1 in first line )
+
+
+    printf("a+b = %d\n" , a==b ); //here is changes  
+    printf("a-b = %d\n" , a-b );
+    printf("a*b = %d\n" , a*b );
+    printf("a/b = %d\n" , a/b );
+
+    return 0;
+
+
+
+}
---
+++ cTuts7e.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int a, b;
+    // let a = 2 and b = 3
+    a = 2;  
+    b = 3;   
+
+
+    printf("a && b = %d\n" ,  a & b ); 
+    return 0;
+
+
+
+}
